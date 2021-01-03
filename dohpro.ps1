071b9404823228ecc4bb1246afbca911a3f8c575
Konrad Iturbe <KonradIT@users.noreply.github.com>
2021-01-03 23:40:49 +0100

    Update dohpro.ps1

--- dohpro.ps1
+++ dohpro.ps1
@@ -2,6 +2,10 @@ Get-ChildItem -Filter GH*.MP4 |
     Rename-Item -NewName { $_.Name -replace 'GH(\d{2})(\d{4})', 'GoProH-$2-$1' }
 Get-ChildItem -Filter GX*.MP4 |
     Rename-Item -NewName { $_.Name -replace 'GX(\d{2})(\d{4})', 'GoProX-$2-$1' }
+Get-ChildItem -Filter GL*.LRV |
+    Rename-Item -NewName { $_.Name -replace 'GX(\d{2})(\d{4})', 'GoProL-$2-$1' }
+
+del *.THM
 
 New-Item -ItemType Directory -Force -Path 'videos'
 New-Item -ItemType Directory -Force -Path 'photos'
