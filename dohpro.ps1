323214d5344bf1db4d3229ea17158e3c624ad661
Konrad Iturbe <KonradIT@users.noreply.github.com>
2021-01-03 23:31:25 +0100

    Can't wait to flash Arch on this thing holy shit

--- dohpro.ps1
+++ dohpro.ps1
@@ -1,2 +1,10 @@
-Get-ChildItem -Filter GH*.mp4 |
-    Rename-Item -NewName { $_.Name -replace 'GH(\d{2})(\d{4})', 'GoPro-$2-$1' }
+Get-ChildItem -Filter GH*.MP4 |
+    Rename-Item -NewName { $_.Name -replace 'GH(\d{2})(\d{4})', 'GoProH-$2-$1' }
+Get-ChildItem -Filter GX*.MP4 |
+    Rename-Item -NewName { $_.Name -replace 'GX(\d{2})(\d{4})', 'GoProX-$2-$1' }
+
+New-Item -ItemType Directory -Force -Path 'videos'
+New-Item -ItemType Directory -Force -Path 'photos'
+
+get-childitem *.MP4,*.LRV,*.THM,*.360 -recurse | foreach-object {move-item $_ -destination videos\}
+get-childitem *.JPG,*.GPR -recurse | foreach-object {move-item $_ -destination photos\}
